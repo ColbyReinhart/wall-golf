904470ddbe2020c4066cb7f744b740c57ec4c29a
pwest22 <pwest7@kent.edu>
2022-10-30 17:34:38 -0400

    adding files for spikes and trampoline

---
+++ Assets/Scripts/MoveableObjects/Spike.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MoveableObject
+{
+    private void Awake()
+    {
+        highlightFactor = 1.15f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if the player collides with the spikes destroy it
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
