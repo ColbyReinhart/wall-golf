14fca59e16080f35fb9c42d5c86e708e6139aa12
Phillip West <90290517+pwest22@users.noreply.github.com>
2022-10-30 18:14:35 -0400

    adding files for spikes and trampoline (#23)
    * adding files for spikes and trampoline
    
    * fixing requested files

---
+++ Assets/Scripts/MoveableObjects/Trampoline.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MoveableObject
+{
+    private void Awake()
+    {
+        highlightFactor = 1.15f;
+    }
+}
