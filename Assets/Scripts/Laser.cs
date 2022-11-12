7eaee5ea7c6d52e9d7815b167d3c29a2ced57389
pwest22 <pwest7@kent.edu>
2022-11-12 12:02:49 -0500

    implemented laser

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //if the player collides with the laser destroy it
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class LaserController : MonoBehaviour
+{
+    public MeshRenderer laser;
+    bool OnOff = true;
+    private float laserSwitch = 5;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        laserSwitch -= Time.deltaTime;
+        double switchTime = Math.Ceiling(laserSwitch);
+        if (switchTime == 0)
+        {
+            laserSwitch = 5;
+            OnOff = !OnOff;
+            laser.gameObject.SetActive(OnOff);
+        }
+    }
+}
