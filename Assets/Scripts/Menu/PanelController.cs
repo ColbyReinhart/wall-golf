c57fad8b3935053d9733ac5b1403866e3e5e5bd8
Colby Reinhart <56095984+ColbyReinhart@users.noreply.github.com>
2022-11-15 17:20:22 -0500

    Implement level system (#27)
    Implemented level and menu systems.

---
+++ Assets/Scripts/Menu/PanelController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+    private GameObject levelClearPanel;
+    private GameObject pausePanel;
+
+    void Start()
+    {
+        // Get panels
+        levelClearPanel = GameObject.Find("LevelClearPanel");
+        pausePanel = GameObject.Find("PausePanel");
+
+        // Set panels to inactive
+        levelClearPanel.SetActive(false);
+        pausePanel.SetActive(false);
+    }
+
+    public void OpenLevelClearPanel()
+    {
+        levelClearPanel.SetActive(true);
+    }
+
+    public void TogglePausePanel(bool paused)
+    {
+        pausePanel.SetActive(paused);
+    }
+}
--- Assets/Scripts/WinZone.cs
+++ Assets/Scripts/WinZone.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class WinZone : MonoBehaviour
 {
+    public PanelController panelController;
+    public LevelLoader levelLoader;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("You won! :D");
+            // The player shouldn't be able to interact with the
+            // level after they win.
+            GameObject.Find("InputController").SetActive(false);
+
+            // Open the level clear panel
+            panelController.OpenLevelClearPanel();
+
+            // Advance the atLevel player pref, if applicable
+            int atLevel = PlayerPrefs.GetInt("atLevel", 1);
+            if (atLevel == levelLoader.currentLevel)
+            {
+                PlayerPrefs.SetInt("atLevel", atLevel + 1);
+            }
         }
     }
 }
