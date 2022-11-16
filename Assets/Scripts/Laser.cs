75aa5f44d6b3566900feadf61f8cb508c958d52b
pwest22 <pwest7@kent.edu>
2022-11-15 22:22:27 -0500

    implemented requested changes

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -1,9 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
 public class Laser : MonoBehaviour
 {
+    MeshRenderer laserTexture;
+    BoxCollider laserHitbox;
+    private bool OnOff = true;
+    private bool beginLaser = false;
+    public float laserSwitch = 3f;
+    private float lst;
+
+    private void Start()
+    {
+        laserTexture = GetComponent<MeshRenderer>();
+        laserHitbox = GetComponent<BoxCollider>();
+        lst = laserSwitch;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Space")) { beginLaser = !beginLaser; }
+        Debug.Log(beginLaser);
+        if (beginLaser == true) {
+            lst -= Time.deltaTime;
+            double switchTime = Math.Ceiling(lst);
+            if (switchTime == 0)
+            {
+                lst = laserSwitch;
+                OnOff = !OnOff;
+                laserTexture.enabled = OnOff;
+                laserHitbox.enabled = OnOff;
+            }
+        } else { laserTexture.enabled = true; laserHitbox.enabled = true; }
+    }
     private void OnTriggerEnter(Collider other)
     {
         //if the player collides with the laser destroy it
