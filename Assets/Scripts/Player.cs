eed9b51c8e4ab6a6d1be4b55569cd74e26317a1b
pwest22 <pwest7@kent.edu>
2022-12-05 20:16:24 -0500

    added 2 new levels, added full-button functionality for menus, fixed ball angular velocity not being reset (i think), fixed spike collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Vector3 initialPos = new Vector3(0, 0, 0);
+    private Rigidbody rb;
 
     private void Awake()
     {
         initialPos = transform.position;
+        rb = GetComponent<Rigidbody>();
     }
 
     public void resetPosition()
     {
         transform.position = initialPos;
+        rb.angularVelocity = Vector3.zero;
     }
 }
