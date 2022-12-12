c991291f2cf503edeed81bfa7edcad8be292e52d
ColbyReinhart <colbyreinhart@gmail.com>
2022-12-12 17:02:12 -0500

    Added game over menu functionality

--- Assets/Scripts/Menu/PanelController.cs
+++ Assets/Scripts/Menu/PanelController.cs
@@ -6,21 +6,29 @@ public class PanelController : MonoBehaviour
 {
     private GameObject levelClearPanel;
     private GameObject pausePanel;
+    private GameObject gameOverPanel;
 
     void Start()
     {
         // Get panels
         levelClearPanel = GameObject.Find("LevelClearPanel");
         pausePanel = GameObject.Find("PausePanel");
+        gameOverPanel = GameObject.Find("GameOverPanel");
 
         // Set panels to inactive
         levelClearPanel.SetActive(false);
         pausePanel.SetActive(false);
+        gameOverPanel.SetActive(false);
     }
 
     public void OpenLevelClearPanel()
     {
         levelClearPanel.SetActive(true);
+    }
+
+    public void OpenGameOverPanel()
+    {
+        gameOverPanel.SetActive(true);
     }
 
     public void TogglePausePanel(bool paused)
