3fe6e7760fe4df85fa2a394a9d4b373ac7d264da
Colby Reinhart <56095984+ColbyReinhart@users.noreply.github.com>
2022-12-13 23:00:48 -0500

    Rework textures (#43)
    Reworked texture system in MoveableObject component.

--- Assets/Scripts/MoveableObject.cs
+++ Assets/Scripts/MoveableObject.cs
@@ -4,51 +4,15 @@ using UnityEngine;
 
 public class MoveableObject : MonoBehaviour
 {
-    public Material highlightMat;
+    public GameObject highlight;
+
     private InputController inputController;
-
     private bool isSelected;
-    private GameObject highlight;
     private MoveableObjController.WorldBounds bounds;
-    protected float highlightFactor = 1.05f;
 
-    // We achieve highlighting without having to mess with shaders
-    // through this hack. A highlight is just a copy of the object
-    // moved slightly behind it, scaled slightly larger, and colored
-    // with a different material. We make this highlight a child of
-    // the actual MoveableObject so that it follows the MoveableObject.
     void Start()
     {
-        // Get inputController reference
         inputController = GameObject.Find("InputController").GetComponent<InputController>();
-
-        // Make a new empty game object which is a child of the moveable object
-        highlight = new GameObject();
-        highlight.transform.SetParent(this.transform);
-        Debug.Assert(highlight.transform.parent == this.transform);
-
-        // Make it the same shape as the parent, with the "highlight" material
-        highlight.AddComponent<MeshRenderer>();
-        highlight.AddComponent<MeshFilter>();
-        highlight.GetComponent<MeshRenderer>().material = highlightMat;
-        highlight.GetComponent<MeshFilter>().mesh = this.GetComponent<MeshFilter>().mesh;
-
-        // Preserve position and rotation
-        highlight.transform.position = transform.position;
-        highlight.transform.rotation = transform.rotation;
-
-        // Make it slightly larger than the parent
-        Vector3 parentDimensions = this.GetComponent<Collider>().bounds.size;
-        float widthRatio = (transform.localScale.x / transform.localScale.y);
-        highlight.transform.localScale = new Vector3(highlightFactor, (highlightFactor - 1f) * widthRatio + 1f, 1f);
-
-        // Move it behind the parent
-        Vector3 oldPos = transform.position;
-        highlight.transform.position = new Vector3(oldPos.x, oldPos.y, oldPos.z + 0.1f);
-
-        // The highlight won't initially appear
-        highlight.SetActive(false);
-        Debug.Assert(highlight.transform.parent == this.transform);
     }
 
     private void OnMouseDrag()
@@ -79,7 +43,7 @@ public class MoveableObject : MonoBehaviour
     public void Deselect()
     {
         isSelected = false;
-        highlight.SetActive(isSelected);
+        highlight.SetActive(false);
     }
 
     // This sets the world bounds "according to the moveable object", meaning that
