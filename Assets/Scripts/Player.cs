d4fda5121bd7fccec532de6db91e823231f68ec9
Colby Reinhart <colbyreinhart@gmail.com>
2022-12-15 23:08:33 -0500

    Improved ball physics in response to switching modes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,24 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Vector3 initialPos = new Vector3(0, 0, 0);
+    private Vector3 initialPos;
     private Rigidbody rb;
-    private Collider col;
 
     private void Awake()
     {
         initialPos = transform.position;
         rb = GetComponent<Rigidbody>();
-        col = GetComponent<Collider>();
     }
 
     public void resetPosition(bool playMode)
     {
         // Reset phyics
-        rb.useGravity = playMode;
-        rb.velocity = Vector3.zero;
-        col.enabled = playMode;
+        rb.isKinematic = !playMode;
         transform.position = initialPos;
-        rb.angularVelocity = Vector3.zero;
+        transform.rotation = Quaternion.identity;
     }
 }
